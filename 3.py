64efd30b3b0045df5cc9ba9fe8b350d52d39a6e5
thepsychiccweeb <153130068+thepsychiccweeb@users.noreply.github.com>
2024-06-20 18:13:32 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -2,6 +2,9 @@ import tensorflow as tf
 from tensorflow.keras.applications import vgg19
 from tensorflow.keras.models import Model
 from tensorflow.keras import backend as K
+from PIL import Image
+import numpy as np
+import streamlit as st
 
 def get_model():
     vgg = vgg19.VGG19(include_top=False, weights='imagenet')
@@ -18,20 +21,15 @@ def get_feature_representations(model, content_path, style_path):
     style_image = load_and_process_img(style_path)
     content_outputs = model(content_image)
     style_outputs = model(style_image)
-    content_features = [content_layer[0] for content_layer in content_outputs[:1]]
-    style_features = [style_layer[0] for style_layer in style_outputs[1:]]
+    content_features = [content_layer for content_layer in content_outputs[:1]]
+    style_features = [style_layer for style_layer in style_outputs[1:]]
     return content_features, style_features
 
 def gram_matrix(input_tensor):
-    channels = int(input_tensor.shape[-1])
-    a = tf.reshape(input_tensor, [-1, channels])
-    n = tf.shape(a)[0]
-    gram = tf.matmul(a, a, transpose_a=True)
-    return gram / tf.cast(n, tf.float32)
-
-from PIL import Image
-import numpy as np
-import tensorflow as tf
+    result = tf.linalg.einsum('bijc,bijd->bcd', input_tensor, input_tensor)
+    input_shape = tf.shape(input_tensor)
+    num_locations = tf.cast(input_shape[1]*input_shape[2], tf.float32)
+    return result / num_locations
 
 def load_img(path_to_img):
     max_dim = 512
@@ -70,7 +68,8 @@ def compute_loss(model, loss_weights, init_image, gram_style_features, content_f
     content_score = 0
     weight_per_style_layer = 1.0 / float(len(style_layers))
     for target_style, comb_style in zip(gram_style_features, style_output_features):
-        style_score += weight_per_style_layer * tf.reduce_mean(tf.square(comb_style - target_style))
+        gram_comb_style = gram_matrix(comb_style)
+        style_score += weight_per_style_layer * tf.reduce_mean(tf.square(gram_comb_style - target_style))
     content_score += tf.reduce_mean(tf.square(content_output_features[0] - content_features[0]))
     style_score *= loss_weights[1]
     content_score *= loss_weights[0]
@@ -118,8 +117,6 @@ def style_transfer(content_path, style_path, num_iterations=1000, content_weight
         if i % 100 == 0:
             print('Iteration: {}'.format(i))        
     return best_img
-
-import streamlit as st
 
 st.title("Neural Style Transfer with Streamlit")
 
